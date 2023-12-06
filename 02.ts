8cc60cbd95609708169a5ea3d816b8ebcc9926a1
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-06 17:06:05 +0100

    Day 2, part 2

--- 02.ts
+++ 02.ts
@@ -6,26 +6,29 @@ if (lines.length > 0 && lines[lines.length - 1] === '') {
     lines.pop();
 }
 
-let sum = 0;
+let part1 = 0;
+let part2 = 0;
 for (const line of lines) {
     const [game, sets_str] = line.split(": ");
     assert(game.startsWith("Game "));
     const game_id = parseInt(game.slice(5));
-    let possible = true;
+    const min_required = new Map<string, number>();
     for (const set_str of sets_str.split("; ")) {
         const color_to_cnt = new Map<string, number>();
         for (const item of set_str.split(", ")) {
             const [count, color] = item.split(" ");
             assert(!color_to_cnt.has(color));
             color_to_cnt.set(color, parseInt(count));
+            min_required.set(color, Math.max(min_required.get(color) ?? 0, parseInt(count)));
         }
-        possible = possible &&
-            (color_to_cnt.get("red") ?? 0) <= 12 &&
-            (color_to_cnt.get("green") ?? 0) <= 13 &&
-            (color_to_cnt.get("blue") ?? 0) <= 14;
-        }
-    if (possible) {
-        sum += game_id;
     }
+    const num_red = min_required.get("red") ?? 0;
+    const num_green = min_required.get("green") ?? 0;
+    const num_blue = min_required.get("blue") ?? 0;
+    if (num_red <= 12 && num_green <= 13 && num_blue <= 14) {
+        part1 += game_id;
+    }
+    part2 += num_red * num_green * num_blue;
 }
-console.log("Part 1:", sum);
+console.log("Part 1:", part1);
+console.log("Part 2:", part2);
