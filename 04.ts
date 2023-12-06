e06b5b959125680f350a2e5e994ac0b9fdae6adf
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-06 18:41:46 +0100

    Day 4, part 2

--- 04.ts
+++ 04.ts
@@ -5,22 +5,29 @@ let lines = data.split("\n");
 assert(lines.pop() === "");
 
 let part1 = 0;
+let part2 = 0;
+let num_copies = lines.map(() => 1);
+let i = 0;
 for (let line of lines) {
+    part2 += num_copies[i];
     let [_, parts] = line.split(": ");
     let [winning_str, my_str] = parts.split("|");
     let winning = winning_str.trim().split(/\s+/g).map(x => parseInt(x));
     let my = my_str.trim().split(/\s+/g).map(x => parseInt(x));
-    let score = 0;
+    let num_matches = 0;
     for (let x of my) {
         if (winning.includes(x)) {
-            if (score === 0) {
-                score = 1;
-            } else {
-                score *= 2;
-            }
+            num_matches += 1;
         }
     }
-    part1 += score;
+    if (num_matches > 0) {
+        part1 += 1 << (num_matches - 1);
+    }
+    for (let j = i + 1; j < i + 1 + num_matches; j++) {
+        num_copies[j] += num_copies[i];
+    }
+    i += 1;
 }
 
 console.log("Part 1:", part1);
+console.log("Part 2:", part2);
