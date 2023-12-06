c2d4b92dc31d09ca3f283c399919c88b5c314b60
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-06 18:33:45 +0100

    Tweak template

--- template.ts
+++ template.ts
@@ -1,11 +1,9 @@
 import { assert } from "https://deno.land/std@0.208.0/assert/assert.ts";
 
-const data = new TextDecoder("utf-8").decode(await Deno.readFile("data/<day>.in"));
-const lines = data.split("\n");
-if (lines.length > 0 && lines[lines.length - 1] === '') {
-    lines.pop();
-}
+let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/<day>.in"));
+let lines = data.split("\n");
+assert(lines.pop() === "");
 
-let part1 = 42;
+let part1 = 0;
 
 console.log("Part 1:", part1);
