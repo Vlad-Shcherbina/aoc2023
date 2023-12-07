85d1470a7c04ba8d9da6ef36b6970928f5a52001
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-07 10:55:38 +0100

    Day 7, minor cleanup

--- 07.ts
+++ 07.ts
@@ -5,7 +5,6 @@ let lines = data.split("\n");
 assert(lines.pop() === "");
 
 let card_strength = "23456789TJQKA";
-let card_strength2 = "J23456789TQKA";
 function hand_strength(hand: string) {
     let cnts_map = new Map<string, number>();
     for (let card of hand) {
@@ -22,12 +21,13 @@ function hand_strength2(hand: string) {
         cnts_map.set(card, (cnts_map.get(card) ?? 0) + 1);
     }
     let j_cnt = cnts_map.get("J") ?? 0;
-    let cnts = [...cnts_map].filter(([card, cnt]) => card !== "J").map(([card, cnt]) => cnt).sort((a, b) => b - a);
+    cnts_map.delete("J");
+    let cnts = [...cnts_map.values()].sort((a, b) => b - a);
     if (cnts.length === 0) {
         cnts.push(0);
     }
     cnts[0] += j_cnt;
-    let card_strengths = [...hand].map(card => card_strength2.indexOf(card));
+    let card_strengths = [...hand].map(card => card === "J" ? -1 : card_strength.indexOf(card));
     return [cnts, card_strengths];
 }
 
