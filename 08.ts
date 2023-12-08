623c2663ab7f731d65fa1c74b3dadb652a178374
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-08 08:50:49 +0100

    Day 8, part 1

--- 08.ts
+++ 08.ts
@@ -4,6 +4,31 @@ let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/08.in"));
 let lines = data.split("\n");
 assert(lines.pop() === "");
 
-let part1 = 0;
+let directions = lines[0];
+let nodes = lines.slice(2).map(line => {
+    // "AAA = (BBB, CCC)"
+    let [name, rest] = line.split(" = (");
+    assert(rest.endsWith(")"));
+    rest = rest.slice(0, -1);
+    let [left, right] = rest.split(", ");
+    return {name, left, right};
+});
+let line_to_node = new Map<string, {name: string, left: string, right: string}>();
+nodes.forEach(node => {
+    line_to_node.set(node.name, node);
+});
 
+let part1 = 0;
+let cur = 'AAA';
+while (cur !== 'ZZZ') {
+    let dir = directions[part1 % directions.length];
+    if (dir === 'L') {
+        cur = line_to_node.get(cur)!.left;
+    } else if (dir === 'R') {
+        cur = line_to_node.get(cur)!.right;
+    } else {
+        assert(false);
+    }
+    part1 += 1;
+}
 console.log("Part 1:", part1);
