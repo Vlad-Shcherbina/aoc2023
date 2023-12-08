f3a3c54069e87d2a80415425e8f50e7fec040896
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-08 09:01:55 +0100

    Day 8, part 2

--- 08.ts
+++ 08.ts
@@ -1,5 +1,20 @@
 import { assert } from "https://deno.land/std@0.208.0/assert/assert.ts";
 
+function gcd2(x: number, y: number) {
+    while (y !== 0) {
+        [x, y] = [y, x % y];
+    }
+    return x;
+}
+
+function lcm2(x: number, y: number) {
+    return x * y / gcd2(x, y);
+}
+
+function lcm(...nums: number[]) {
+    return nums.reduce(lcm2);
+}
+
 let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/08.in"));
 let lines = data.split("\n");
 assert(lines.pop() === "");
@@ -18,17 +33,26 @@ nodes.forEach(node => {
     line_to_node.set(node.name, node);
 });
 
-let part1 = 0;
-let cur = 'AAA';
-while (cur !== 'ZZZ') {
-    let dir = directions[part1 % directions.length];
-    if (dir === 'L') {
-        cur = line_to_node.get(cur)!.left;
-    } else if (dir === 'R') {
-        cur = line_to_node.get(cur)!.right;
-    } else {
-        assert(false);
-    }
-    part1 += 1;
+for (let part of [1, 2]) {
+    let start_nodes = part == 1 ? ["AAA"] : nodes.map(node => node.name).filter(name => name.endsWith("A"));
+    console.log(start_nodes);
+    let is_end_node = part == 1 ? (name: string) => name === "ZZZ" : (name: string) => name.endsWith("Z");
+
+    let lens = start_nodes.map(name => {
+        let i = 0;
+        let cur = name;
+        while (!is_end_node(cur)) {
+            let dir = directions[i % directions.length];
+            if (dir === "L") {
+                cur = line_to_node.get(cur)!.left;
+            } else if (dir === "R") {
+                cur = line_to_node.get(cur)!.right;
+            } else {
+                assert(false);
+            }
+            i += 1;
+        }
+        return i;
+    });
+    console.log(`Part ${part}:`, lcm(...lens));
 }
-console.log("Part 1:", part1);
