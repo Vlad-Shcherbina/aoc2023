564e17b468118d3e8fe6b3fe9970477f8939417b
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-09 09:42:44 +0100

    Day 9, part 1

--- 09.ts
+++ 09.ts
@@ -4,6 +4,28 @@ let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/09.in"));
 let lines = data.split("\n");
 assert(lines.pop() === "");
 
-let part1 = 0;
+function predict(xs: number[]) {
+    let diffss = [xs];
+    while (true) {
+        let last_diffs = diffss[diffss.length - 1];
+        if (last_diffs.every(x => x === 0)) {
+            break;
+        }
+        let new_diffs = last_diffs.slice(1).map((x, i) => x - last_diffs[i]);
+        diffss.push(new_diffs);
+    }
+    diffss[diffss.length - 1].push(0);
+    for (let i = diffss.length - 1; i > 0; i--) {
+        let diffs = diffss[i - 1];
+        let prev_diffs = diffss[i];
+        diffs.push(diffs[diffs.length - 1] + prev_diffs[prev_diffs.length - 1]);
+    }
+    return diffss[0][diffss[0].length - 1];
+}
 
+let part1 = 0;
+for (let line of lines) {
+    let xs = line.split(/\s+/).map(x => parseInt(x));
+    part1 += predict(xs);
+}
 console.log("Part 1:", part1);
