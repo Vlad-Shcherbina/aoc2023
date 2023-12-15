2bc6e6b253a69ce079a26e51262742a3b3192b17
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-15 10:39:10 +0100

    Day 15 init

---
+++ 15.ts
@@ -0,0 +1,9 @@
+import { assert } from "https://deno.land/std@0.208.0/assert/assert.ts";
+
+let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/15.in"));
+let lines = data.split("\n");
+assert(lines.pop() === "");
+
+let part1 = 0;
+
+console.log("Part 1:", part1);
