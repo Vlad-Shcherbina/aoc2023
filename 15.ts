bc348867a72af3c55ac007f87a60b9c9205f532e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-15 11:41:24 +0100

    Day 15, part 2

--- 15.ts
+++ 15.ts
@@ -21,3 +21,34 @@ for (let part of lines[0].split(",")) {
     part1 += hash(part);
 }
 console.log("Part 1:", part1);
+
+let boxes: {label: string, focus: number}[][] = Array.from({length: 256}, () => []);
+for (let part of lines[0].split(",")) {
+    if (part.indexOf("=") !== -1) {
+        let [label, focus_s] = part.split("=");
+        let focus = parseInt(focus_s);
+        let found = false;
+        let box = boxes[hash(label)];
+        for (let len of box) {
+            if (len.label === label) {
+                len.focus = focus;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            box.push({label, focus});
+        }
+    } else {
+        assert(part.endsWith("-"));
+        let label = part.slice(0, -1);
+        boxes[hash(label)] = boxes[hash(label)].filter((box) => box.label !== label);
+    }
+}
+let part2 = 0;
+boxes.forEach((box, box_idx) => {
+    box.forEach((len, len_idx) => {
+        part2 += (box_idx + 1) * (len_idx + 1) * len.focus;
+    })
+});
+console.log("Part 2:", part2);
