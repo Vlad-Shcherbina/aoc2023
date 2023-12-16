8d1123fadcf758bfb408b0f83b02b447f050069d
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-16 10:56:03 +0100

    Day 16, part 1

--- 16.ts
+++ 16.ts
@@ -4,6 +4,44 @@ let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/16.in"));
 let lines = data.split("\n");
 assert(lines.pop() === "");
 
-let part1 = 0;
+let w = lines[0].length;
+let h = lines.length;
 
-console.log("Part 1:", part1);
+let energized = new Set<string>();
+let queue: [number, number, number, number][] = [[0, 0, 1, 0]];
+let visited = new Set<string>();
+while (queue.length > 0) {
+    let [x, y, dx, dy] = queue.pop()!;
+    let key = `${x},${y},${dx},${dy}`;
+    if (visited.has(key)) continue;
+    visited.add(key);
+
+    if (x < 0 || x >= w || y < 0 || y >= h) continue;
+    energized.add(`${x},${y}`);
+    let c = lines[y][x];
+    if (c === ".") {
+        queue.push([x + dx, y + dy, dx, dy]);
+    } else if (c === "/") {
+        queue.push([x - dy, y - dx, -dy, -dx]);
+    } else if (c === "\\") {
+        queue.push([x + dy, y + dx, dy, dx]);
+    } else if (c === "-") {
+        if (dy === 0) {
+            queue.push([x + dx, y + dy, dx, dy]);
+        } else {
+            queue.push([x - 1, y, -1, 0]);
+            queue.push([x + 1, y, 1, 0]);
+        }
+    } else if (c === "|") {
+        if (dx === 0) {
+            queue.push([x + dx, y + dy, dx, dy]);
+        } else {
+            queue.push([x, y - 1, 0, -1]);
+            queue.push([x, y + 1, 0, 1]);
+        }
+    } else {
+        assert(false, c);
+    }
+}
+
+console.log("Part 1:", energized.size);
