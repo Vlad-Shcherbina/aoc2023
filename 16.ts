d574121441b890aa9368a613a5c5d724f785dea5
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-16 10:58:51 +0100

    Day 16, part 2

--- 16.ts
+++ 16.ts
@@ -7,41 +7,55 @@ assert(lines.pop() === "");
 let w = lines[0].length;
 let h = lines.length;
 
-let energized = new Set<string>();
-let queue: [number, number, number, number][] = [[0, 0, 1, 0]];
-let visited = new Set<string>();
-while (queue.length > 0) {
-    let [x, y, dx, dy] = queue.pop()!;
-    let key = `${x},${y},${dx},${dy}`;
-    if (visited.has(key)) continue;
-    visited.add(key);
+function count_energized(x: number, y: number, dx: number, dy: number) {
+    let energized = new Set<string>();
+    let queue: [number, number, number, number][] = [[x, y, dx, dy]];
+    let visited = new Set<string>();
+    while (queue.length > 0) {
+        let [x, y, dx, dy] = queue.pop()!;
+        let key = `${x},${y},${dx},${dy}`;
+        if (visited.has(key)) continue;
+        visited.add(key);
 
-    if (x < 0 || x >= w || y < 0 || y >= h) continue;
-    energized.add(`${x},${y}`);
-    let c = lines[y][x];
-    if (c === ".") {
-        queue.push([x + dx, y + dy, dx, dy]);
-    } else if (c === "/") {
-        queue.push([x - dy, y - dx, -dy, -dx]);
-    } else if (c === "\\") {
-        queue.push([x + dy, y + dx, dy, dx]);
-    } else if (c === "-") {
-        if (dy === 0) {
+        if (x < 0 || x >= w || y < 0 || y >= h) continue;
+        energized.add(`${x},${y}`);
+        let c = lines[y][x];
+        if (c === ".") {
             queue.push([x + dx, y + dy, dx, dy]);
+        } else if (c === "/") {
+            queue.push([x - dy, y - dx, -dy, -dx]);
+        } else if (c === "\\") {
+            queue.push([x + dy, y + dx, dy, dx]);
+        } else if (c === "-") {
+            if (dy === 0) {
+                queue.push([x + dx, y + dy, dx, dy]);
+            } else {
+                queue.push([x - 1, y, -1, 0]);
+                queue.push([x + 1, y, 1, 0]);
+            }
+        } else if (c === "|") {
+            if (dx === 0) {
+                queue.push([x + dx, y + dy, dx, dy]);
+            } else {
+                queue.push([x, y - 1, 0, -1]);
+                queue.push([x, y + 1, 0, 1]);
+            }
         } else {
-            queue.push([x - 1, y, -1, 0]);
-            queue.push([x + 1, y, 1, 0]);
+            assert(false, c);
         }
-    } else if (c === "|") {
-        if (dx === 0) {
-            queue.push([x + dx, y + dy, dx, dy]);
-        } else {
-            queue.push([x, y - 1, 0, -1]);
-            queue.push([x, y + 1, 0, 1]);
-        }
-    } else {
-        assert(false, c);
     }
+    return energized.size;
 }
 
-console.log("Part 1:", energized.size);
+console.log("Part 1:", count_energized(0, 0, 1, 0));
+
+let part2 = 0;
+for (let x = 0; x < w; x++) {
+    part2 = Math.max(part2, count_energized(x, 0, 0, 1));
+    part2 = Math.max(part2, count_energized(x, h - 1, 0, -1));
+}
+for (let y = 0; y < h; y++) {
+    part2 = Math.max(part2, count_energized(0, y, 1, 0));
+    part2 = Math.max(part2, count_energized(w - 1, y, -1, 0));
+}
+console.log("Part 2:", part2);
