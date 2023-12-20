b8e8ea00a4f9ad68946bfc2b4080286bcbc5e896
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-20 12:58:08 +0100

    Day 10, part 2, semi-manual, quick and dirty

--- 20.ts
+++ 20.ts
@@ -54,11 +54,21 @@ for (let [name, module] of modules) {
     }
 }
 
+/*let dot = "digraph {\n";
+for (let [name, module] of modules) {
+    dot += `    ${name} [label="${module.type + name}"];\n`;
+    for (let dst of module.connections) {
+        dot += `    ${name} -> ${dst};\n`;
+    }
+}
+dot += "}\n";
+await Deno.writeTextFile("20.dot", dot);*/
+
 let pulses1: [string, boolean, string][] = [];
 let pulses2: [string, boolean, string][] = [];
 let cnt_low = 0;
 let cnt_high = 0;
-for (let i = 0; i < 1000; i++) {
+for (let i = 0; i < 10000; i++) {
     pulses1.push(["button", false, "broadcaster"]);
     while (true) {
         if (pulses2.length === 0) {
@@ -70,7 +80,16 @@ for (let i = 0; i < 1000; i++) {
             break;
         }
         let [sender_name, is_high, module_name] = pulses2.pop()!;
-        // console.log(sender_name, is_high, module_name);
+        // let interesting = ["vd", "bh", "dl", "ns"];
+        // if (interesting.includes(module_name)) {
+        //     if (is_high) {
+        //         console.log(module_name, i);
+        //     }
+        // }
+        // if (module_name === "rx" && !is_high) {
+        //     console.log(sender_name, is_high, module_name, i + 1);
+        //     assert(false);
+        // }
         if (is_high) {
             cnt_high += 1;
         } else {
@@ -90,6 +109,21 @@ for (let i = 0; i < 1000; i++) {
         } else if (module.type === "&") {
             assert(module.incoming.has(sender_name));
             module.incoming.set(sender_name, is_high);
+            if (module_name === "zh") {
+                // let any_high = false;
+                for (let [input_name, is_high] of module.incoming) {
+                    if (is_high) {
+                        console.log(i, input_name);
+                    }
+                    // if (is_high) {
+                    //     any_high = true;
+                    //     break;
+                    // }
+                }
+                // if (any_high) {
+                //     console.log(i, module.incoming);
+                // }
+            }
             let all_high = true;
             for (let [_, is_high] of module.incoming) {
                 if (!is_high) {
@@ -111,3 +145,26 @@ for (let i = 0; i < 1000; i++) {
 }
 
 console.log("Part 1:", cnt_low * cnt_high);
+
+function gcd2(x: number, y: number) {
+    while (y !== 0) {
+        [x, y] = [y, x % y];
+    }
+    return x;
+}
+
+function lcm2(x: number, y: number) {
+    return x * y / gcd2(x, y);
+}
+
+function lcm(...nums: number[]) {
+    return nums.reduce(lcm2);
+}
+
+/*
+bh: 3760 7521 11282 15043
+ns: 3766 7533 11300 15067
+dl: 3778 7557 11336 15115
+vd: 3880 7761 11642 15523
+*/
+console.log("Part 2:", lcm(3761, 3767, 3779, 3881));
