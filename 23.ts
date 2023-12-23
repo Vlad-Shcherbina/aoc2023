114874fb8d7d9ef1cec573278bc0e932560fffbf
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2023-12-23 10:34:06 +0100

    Day 23, part 2 (very slow, ~10 min, but likely faster than implementing an advanced algo)

--- 23.ts
+++ 23.ts
@@ -1,3 +1,6 @@
+// Run as
+//   deno run --allow-read --v8-flags=--stack-size=10000 23.ts
+
 import { assert } from "https://deno.land/std@0.208.0/assert/assert.ts";
 
 let data = new TextDecoder("utf-8").decode(await Deno.readFile("data/23.in"));
@@ -7,47 +10,89 @@ assert(lines.pop() === "");
 let w = lines.length;
 let h = lines[0].length;
 
-let visited = new Set<string>();
-visited.add("0,1");
-visited.add("1,1");
-let path: [number, number][] = [[1, 1]];
+let visited = Array.from({ length: w }, () => new Array(h).fill(false));
+visited[0][1] = true;
+visited[1][1] = true;
 
-let part1 = 0;
-function rec() {
-    let [i, j] = path[path.length - 1];
+let deltas: [number, number][] = [[0, 1], [1, 0], [0, -1], [-1, 0]];
+
+function reachable(i: number, j: number) {
+    let q: [number, number][] = [[i, j]];
+    let visited2 = Array.from({ length: w }, () => new Array(h).fill(false));
+    visited2[i][j] = true;
+    while (q.length > 0) {
+        let [i, j] = q.pop()!;
+        if (i == h - 1) {
+            return true;
+        }
+        for (let [di, dj] of deltas) {
+            let i2 = i + di;
+            let j2 = j + dj;
+            if (visited[i2][j2] || visited2[i2][j2]) {
+                continue;
+            }
+            if (lines[i2][j2] === "#") {
+                continue;
+            }
+            visited2[i2][j2] = true;
+            q.push([i2, j2]);
+        }
+    }
+    return false;
+}
+
+function rec(i: number, j: number, length: number) {
     if (i === h - 1) {
-        part1 = Math.max(part1, path.length);
+        if (length > longest) {
+            console.log(length, "...");
+        }
+        longest = Math.max(longest, length);
         return;
     }
     let c = lines[i][j];
     assert(c !== "#");
-    let deltas: [number, number][];
-    if (c === ".") {
-        deltas = [[0, 1], [0, -1], [1, 0], [-1, 0]];
-    } else if (c === ">") {
-        deltas = [[0, 1]];
-    } else if (c === "v") {
-        deltas = [[1, 0]];
-    } else {
-        assert(false, c);
-    }
+    let cnt = 0;
     for (let [di, dj] of deltas) {
+        if (c === ".") {
+            ;
+        } else if (c === ">") {
+            if (dj != 1) {
+                continue;
+            }
+        } else if (c === "v") {
+            if (di != 1) {
+                continue;
+            }
+        } else {
+            assert(false, c);
+        }
         let i2 = i + di;
         let j2 = j + dj;
-        let key = `${i2},${j2}`;
-        if (visited.has(key)) {
+        if (visited[i2][j2]) {
             continue;
         }
         if (lines[i2][j2] === "#") {
             continue;
         }
-        visited.add(key);
-        path.push([i2, j2]);
-        rec();
-        path.pop();
-        visited.delete(key);
+        if (cnt > 0 && !reachable(i2, j2)) {
+            continue;
+        }
+        visited[i2][j2] = true;
+        rec(i2, j2, length + 1);
+        visited[i2][j2] = false;
+        cnt += 1;
     }
 }
-rec();
 
-console.log("Part 1:", part1);
+let start = performance.now();
+
+let longest = 0;
+rec(1, 1, 1);
+console.log("Part 1:", longest);
+
+lines = lines.map((line) => line.replace(/v|>/g, "."));
+longest = 0;
+rec(1, 1, 1);
+console.log("Part 2:", longest);
+
+console.log(`It took ${performance.now() - start} ms`);
